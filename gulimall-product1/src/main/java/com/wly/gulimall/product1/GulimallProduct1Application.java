e3e8908027fd4fae9c3b4310ffb9dfa42022ba5f
wnormal <2169115493@qq.com>
2023-11-03 15:44:35 +0800

    init: 项目初始化结构

---
+++ gulimall-product1/src/main/java/com/wly/gulimall/product1/GulimallProduct1Application.java
@@ -0,0 +1,13 @@
+package com.wly.gulimall.product1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProduct1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProduct1Application.class, args);
+    }
+
+}
---
+++ gulimall-product1/src/test/java/com/wly/gulimall/product1/GulimallProduct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wly.gulimall.product1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProduct1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
